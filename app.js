188fdd8f088d92b9a0909474e8a42a3276e60ea4
ZenKhalil <feattzen@gmail.com>
2023-04-18 08:11:13 +0200

    Created new function preparpostdata

--- app.js
+++ app.js
@@ -12,5 +12,19 @@ const endpoint = "https://new-project-fabad-default-rtdb.europe-west1.firebaseda
 async function getPosts() {
 	const response = await fetch(`${endpoint}/posts.json`); // Posts.json is the data ressource
 	const data = await response.json();
-	console.log(data);
+    const posts = preparePostData(data);
+	console.log(posts);
+    return posts;
+}
+
+function preparePostData(dataObject) {
+const postArray = [];
+for (const key in dataObject) {
+    const post = dataObject[key];
+    post.id = key;
+    console.log(post);
+    postArray.push(post);
+}
+console.log(postArray);
+return postArray;
 }
